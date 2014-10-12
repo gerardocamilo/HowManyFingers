fa91e72c6c3cc3d6aa3cff9aab784b15a7998d71
Gerardo Camilo <gerardocamilo@MacBook-Pro-de-Gerardo.local>
2014-10-12 07:33:17 -0400

    How many fingers app completed

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtInput: UITextField!
+    
+    @IBOutlet weak var lblResult: UILabel!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        var output:String = "Please provide a valid number in the box."
+        
+        if(txtInput.text != nil){
+            
+        var inputNumber = txtInput.text.toInt()
+        
+            if(inputNumber != nil){
+                var randomNum = Int((arc4random() % 6))
+                
+                if(inputNumber == randomNum){
+                    output = "You got it right!"
+                }else{
+                    output = "Wrong! The number was \(randomNum). Try again."
+                }
+        }
+            
+        self.lblResult.text = output
+    }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
